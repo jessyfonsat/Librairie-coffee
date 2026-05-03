5e8ec7c986e8244d10e3de5d6e6120080898de21
Jessy FONSAT <jessy.fonsat@gmail.com>
2026-05-03 18:21:50 +0200

    modif connexion

--- connexion.php
+++ connexion.php
@@ -1,15 +1,23 @@
 <?php
-$host   = "localhost";
-$dbname = "librairie-coffee";
-$user   = "root";
-$pass   = "";
+$is_local = (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false);
+
+if ($is_local) {
+    $host   = "localhost";
+    $port   = "3306";
+    $dbname = "librairie-coffee";
+    $user   = "root";
+    $pass   = "";
+    $dsn    = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+} else {
+    $host   = "sql313.infinityfree.com";
+    $dbname = "if0_41813847_LIBRAIRIE";
+    $user   = "if0_41813847";
+    $pass   = "salut123aze";
+    $dsn    = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+}
 
 try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=3307;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $pass
-    );
+    $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
